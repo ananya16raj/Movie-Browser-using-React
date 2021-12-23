77315dc1f14f18307cdf2c2be27ebddd7c3d711e
Ananya Raj <rajananya16061606@gmail.com>
2021-12-24 03:51:28 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -3,14 +3,13 @@ import Hero from './Hero';
 const Home = () => {
     return (
       <>
-      <Hero text= "Welcome to React 201"/>
-        {/* <h1> Hello World from React 201</h1> */}
+      <Hero text= "Welcome to Movie Mania"/>
+       
         <div className = "container">
           <div className = "row">
             <div className = "col-lg-8 offset-lg-2 my-5">
               <p className ="lead">
-                Hellooooo everyone!!!! Welcome to my Website.
-                I'm developing new skills and this time I affirm to be consistent with my work.
+                Hellooooo Binge-Watchers!!!! Glad you landed here, now grab a bucket loaded with Popcorns, buckle yourself up and GET SET GO!!!!
               </p>
             </div>
           </div>
